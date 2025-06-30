2ec602f2ae18d2b01140dcf4e12e50acf8986230
TegarXLu <botdimas67@gmail.com>
2025-06-30 07:07:53 +0700

    Update xt_rateest.h

--- include/uapi/linux/netfilter/xt_rateest.h
+++ include/uapi/linux/netfilter/xt_rateest.h
@@ -6,12 +6,23 @@
 #include <linux/if.h>
 
 enum xt_rateest_match_flags {
-	XT_RATEEST_MATCH_INVERT	= 1<<0,
-	XT_RATEEST_MATCH_ABS	= 1<<1,
-	XT_RATEEST_MATCH_REL	= 1<<2,
-	XT_RATEEST_MATCH_DELTA	= 1<<3,
-	XT_RATEEST_MATCH_BPS	= 1<<4,
-	XT_RATEEST_MATCH_PPS	= 1<<5,
+    XT_RATEEST_MATCH_INVERT = 1 << 0,
+    XT_RATEEST_MATCH_ABS    = 1 << 1,
+    XT_RATEEST_MATCH_REL    = 1 << 2,
+    XT_RATEEST_MATCH_DELTA  = 1 << 3,
+    XT_RATEEST_MATCH_BPS    = 1 << 4,
+    XT_RATEEST_MATCH_PPS    = 1 << 5,
 };
 
-enu
+struct xt_rateest_match_info {
+    char name1[IFNAMSIZ];
+    char name2[IFNAMSIZ];
+    __u16 bps1;
+    __u16 pps1;
+    __u16 bps2;
+    __u16 pps2;
+    __u8 cmp;
+    __u8 flags;
+};
+
+#endif /* _XT_RATEEST_MATCH_H */
